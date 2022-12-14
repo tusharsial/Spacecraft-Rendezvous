e5e7cad2e47d437897d0af3270977ae58f35ec83
Tushar Sial <f20190215@pilani.bits-pilani.ac.in>
2022-12-14 11:34:06 +0530

    Delete Reference_planet.m

--- Reference_planet.m
+++
@@ -1,7 +0,0 @@
-% This function will define all the parameters of the planet
-
-J2 = 1082.64e-6;    %Second Harmonic due to Earth oblateness effect
-Re = 6.3716e6;      % Radius of Earth
-M = 5.972e24;       % Mass of Earth    
-G = 6.67e-11;       
-myu = G*M;          % Gravitational Constant
